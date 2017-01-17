76890a86e8f1b88de019efdf004b253422909841
fhightower <floyd.hightower27@gmail.com>
2017-01-16 22:46:06 -0400

    Updated the email and user models

--- emailanalysis/models.py
+++ emailanalysis/models.py
@@ -11,3 +11,23 @@ class Email(models.Model):
     recipient_ip = models.GenericIPAddressField()
     sender_email = models.EmailField()
     sender_ip = models.GenericIPAddressField()
+    attachment = models.TextField()
+    submitter = models.CharField("provider", max_length=8)
+
+
+class Attachment(models.Model):
+    """Attachment class for the attachments' table."""
+    # todo: add more stuff here...
+    emails = models.ManyToManyField(Email)
+
+
+class Host(models.Model):
+    """Host class for the hosts' table."""
+    # todo: add more stuff here...
+    emails = models.ManyToManyField(Email)
+
+
+class Url(models.Model):
+    """URL class for the URLs' table."""
+    # todo: add more stuff here...
+    emails = models.ManyToManyField(Email)
--- entrance/models.py
+++ entrance/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """Database model for users."""
+     emails = models.ManyToManyField("emailanalysis.Email",
+                                     verbose_name="related_emails")
+    # field = models.ForeignKey("emailanalysis.Email", verbose_name="email", related_name="my_models2")
+     # todo: add more fields here...
