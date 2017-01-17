405100594dcbb06ef39d0ec0d388037f03b24ced
fhightower <floyd.hightower27@gmail.com>
2017-01-16 20:50:36 -0400

    Added a model for an email

--- emailanalysis/models.py
+++ emailanalysis/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Email(models.Model):
+    """Database model for handling emails."""
+    full_text = models.TextField()
+    header = models.TextField()
+    body = models.TextField()
+    subject = models.CharField(max_length=500)
+    recipient_email = models.EmailField()
+    recipient_ip = models.GenericIPAddressField()
+    sender_email = models.EmailField()
+    sender_ip = models.GenericIPAddressField()
