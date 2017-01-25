0b130594fc4b5d90b407195f328c5ded90ae561c
fhightower <floyd.hightower27@gmail.com>
2017-01-24 22:29:30 -0400

    Added working, basic upload functionality

--- emailanalysis/views.py
+++ emailanalysis/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.views import generic
 
 from .models import Email
@@ -19,9 +16,34 @@ class EmailAnalysisHome(generic.ListView):
         return Email.objects.all()[:5]
 
 
-def upload(request):
-    return render(request, 'emailanalysis/upload.html')
-
 class EmailDetailView(generic.DetailView):
     model = Email
     template_name = "emailanalysis/email-details.html"
+
+
+def upload(request):
+    return render(request, 'emailanalysis/upload.html')
+
+
+def review(request):
+    try:
+        full_email_text = request.POST['full-text']
+        email_subject = request.POST['subject']
+        recipient_email = request.POST['recipient-email']
+        sender_email = request.POST['sender-email']
+        sender_ip_address = request.POST['sender-ip']
+    except (KeyError):
+        # Redisplay the question voting form.
+        # todo: implement an error message
+        # return render(request, 'polls/detail.html', {
+        #     'question': question,
+        #     'error_message': "You didn't select a choice.",
+        # })
+        pass
+    else:
+        # return HttpResponseRedirect(reverse('emailanalysis:review', args=(full_email_text, email_subject, recipient_email, sender_email, sender_ip_address)))
+        context = {
+            'full_email_text': full_email_text,
+            'email_subject': email_subject
+        }
+        return render(request, 'emailanalysis/review.html', context)
