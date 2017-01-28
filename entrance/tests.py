99eb14afb7fc3c404fbe7c820774cf24a8bc9009
fhightower <floyd.hightower27@gmail.com>
2017-01-28 16:52:59 -0400

    Added tests for entrance
    Fixes #5

--- entrance/tests.py
+++ entrance/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ViewTests(TestCase):
+    """View related tests."""
+
+    def test_index_view(self):
+        """Test entrance:index."""
+        response = self.client.get(reverse("entrance:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Welcome to the home page!")
+
+    def test_about_view(self):
+        """Test entrance:about."""
+        response = self.client.get(reverse("entrance:about"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Who, Why, What, and How?")
+
+    def test_invest_view(self):
+        """Test entrance:invest."""
+        response = self.client.get(reverse("entrance:invest"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Upgrade your tools!")
