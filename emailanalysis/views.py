5ac92463920a7f169a579b2ed03842a4c44d9f7e
fhightower <floyd.hightower27@gmail.com>
2017-01-28 19:53:34 -0400

    Working submit example

--- emailanalysis/views.py
+++ emailanalysis/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from django.views import generic
 
 from .models import Email
@@ -26,6 +28,7 @@ def upload(request):
 
 
 def review(request):
+    print("here")
     try:
         full_email_text = request.POST['full-text']
         email_subject = request.POST['subject']
@@ -41,9 +44,6 @@ def review(request):
         # })
         pass
     else:
-        # return HttpResponseRedirect(reverse('emailanalysis:review', args=(full_email_text, email_subject, recipient_email, sender_email, sender_ip_address)))
-        context = {
-            'full_email_text': full_email_text,
-            'email_subject': email_subject
-        }
-        return render(request, 'emailanalysis/review.html', context)
+        new_email = Email(full_text=full_email_text, subject=email_subject, recipient_email=recipient_email, sender_email=sender_email, sender_ip=sender_ip_address, submitter="12345678")
+        new_email.save()
+        return HttpResponseRedirect(reverse('emailanalysis:details', args=(new_email.id,)))
