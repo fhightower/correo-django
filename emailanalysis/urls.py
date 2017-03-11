64ed3a19343fd3aef0766c43824dc8e74e8c4c34
fhightower <floyd.hightower27@gmail.com>
2017-03-11 15:39:20 -0400

    Pass email data into review view

--- emailanalysis/urls.py
+++ emailanalysis/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.EmailAnalysisHome.as_view(), name='index'),
     url(r'^import/$', views.import_, name='import'),
     url(r'^import/parse$', views.parse, name='parse'),
+    url(r'^import/review$', views.review, name='review'),
     url(r'^(?P<pk>[0-9]+)/$', views.EmailDetailView.as_view(), name='details'),
     # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
--- emailanalysis/views.py
+++ emailanalysis/views.py
@@ -1,3 +1,5 @@
+import email
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -26,20 +28,34 @@ class EmailDetailView(generic.DetailView):
 def import_(request):
     return render(request, 'emailanalysis/import.html')
 
+# TODO: this is a temporary hack to get data from the parse view to the review view. In the future, parse out all of the details from the email as done below, render an html page which then submits a get request to the review view with all of the pertinent information
+
+temp_email_data = {
+    'subject': None,
+    'recipient_email': None,
+    'sender_email': None,
+    'sender_ip': None
+}
+
 
 def parse(request):
     try:
         if request.POST.get('full-text'):
             full_email_text = request.POST['full-text']
-            print(full_email_text)
+
         elif request.FILES.get('email-file'):
             email_file = request.FILES['email-file']
-            print(email_file)
+
         # full_email_text = request.POST['full-text']
         # email_subject = request.POST['subject']
         # recipient_email = request.POST['recipient-email']
         # sender_email = request.POST['sender-email']
         # sender_ip_address = request.POST['sender-ip']
+        parsed_email = email.message_from_string(full_email_text)
+        temp_email_data['subject'] = parsed_email.get('subject')
+        temp_email_data['recipient_email'] =  parsed_email.get('to')
+        temp_email_data['sender_email'] = parsed_email.get('from')
+        temp_email_data['sender_ip'] = parsed_email.get('x-originating-ip')
     except KeyError as e:
         # Redisplay the question voting form.
         # todo: implement an error message
@@ -54,4 +70,9 @@ def parse(request):
         # new_email = Email(full_text=full_email_text, subject=email_subject, recipient_email=recipient_email, sender_email=sender_email, sender_ip=sender_ip_address, submitter="12345678")
         # new_email.save()
         # return HttpResponseRedirect(reverse('emailanalysis:details', args=(new_email.id,)))
-        return HttpResponseRedirect(reverse('emailanalysis:index'))
+        return HttpResponseRedirect(reverse('emailanalysis:review'))
+
+def review(request):
+    """."""
+    print("here: {}".format(request))
+    return render(request, 'emailanalysis/review.html', temp_email_data)
