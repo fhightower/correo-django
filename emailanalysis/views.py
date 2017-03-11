587a9648959d442adc5123c0f5c893f119e50a63
fhightower <floyd.hightower27@gmail.com>
2017-03-11 10:26:39 -0400

    Simple upload system with file upload and text field working

--- emailanalysis/views.py
+++ emailanalysis/views.py
@@ -29,20 +29,29 @@ def import_(request):
 
 def submit(request):
     try:
-        full_email_text = request.POST['full-text']
-        email_subject = request.POST['subject']
-        recipient_email = request.POST['recipient-email']
-        sender_email = request.POST['sender-email']
-        sender_ip_address = request.POST['sender-ip']
-    except (KeyError):
+        if request.POST.get('full-text'):
+            full_email_text = request.POST['full-text']
+            print(full_email_text)
+        elif request.FILES.get('email-file'):
+            email_file = request.FILES['email-file']
+            print(email_file)
+        # full_email_text = request.POST['full-text']
+        # email_subject = request.POST['subject']
+        # recipient_email = request.POST['recipient-email']
+        # sender_email = request.POST['sender-email']
+        # sender_ip_address = request.POST['sender-ip']
+    except KeyError as e:
         # Redisplay the question voting form.
         # todo: implement an error message
         # return render(request, 'polls/detail.html', {
         #     'question': question,
         #     'error_message': "You didn't select a choice.",
         # })
-        pass
+        print("Error: {}".format(e))
     else:
-        new_email = Email(full_text=full_email_text, subject=email_subject, recipient_email=recipient_email, sender_email=sender_email, sender_ip=sender_ip_address, submitter="12345678")
-        new_email.save()
-        return HttpResponseRedirect(reverse('emailanalysis:details', args=(new_email.id,)))
+        # email_file = request.FILES.get('file')
+        # print(email_file)
+        # new_email = Email(full_text=full_email_text, subject=email_subject, recipient_email=recipient_email, sender_email=sender_email, sender_ip=sender_ip_address, submitter="12345678")
+        # new_email.save()
+        # return HttpResponseRedirect(reverse('emailanalysis:details', args=(new_email.id,)))
+        return HttpResponseRedirect(reverse('emailanalysis:index'))
